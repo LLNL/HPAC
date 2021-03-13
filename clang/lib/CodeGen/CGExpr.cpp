0edea5e0911ce9ce6d948b7c181ad0ffca423241
Giorgis Georgakoudis <georgakoudis1@llnl.gov>
2021-03-13 09:28:13 -0800

    Support perfo with omp parallel for

--- clang/lib/CodeGen/CGExpr.cpp
+++ clang/lib/CodeGen/CGExpr.cpp
@@ -2845,6 +2845,9 @@ LValue CodeGenFunction::EmitDeclRefLValue(const DeclRefExpr *E) {
 
     // No other cases for now.
     } else {
+      llvm::dbgs() << "Failed decl " << VD->getName() << "\n";
+      VD->dump();
+      llvm::dbgs() << "End of failed decl\n";
       llvm_unreachable("DeclRefExpr for Decl not entered in LocalDeclMap?");
     }
 
