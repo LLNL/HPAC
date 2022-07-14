973d70f1fd204a25b529d7ac84b7c1a77b2b83d5
Zane Fink <finkzane@gmail.com>
2022-07-14 09:12:22 -0500

    Include debug header

---
+++ approx/runtime/src/include/approx_debug.h
@@ -0,0 +1,22 @@
+ //===--- approx_debug.h - Approx debugging API ----------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+/// \file
+/// This file defines the accessible API for development/debugging of the approx runtime system
+///
+//===----------------------------------------------------------------------===//
+
+
+#include <stdbool.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+void resetDeviceTable(int newSize = -1);
+#ifdef __cplusplus
+}
+#endif
